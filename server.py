bf4ee1b8118fd10291eb1589c74c9413ac088248
manel <manelzaum@icloud.com>
2025-01-02 16:44:59 -0300

    add: server to handle requests.

---
+++ server.py
@@ -0,0 +1,26 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from sys import argv
+
+
+class RequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if (
+            not self.path.endswith(".js")
+            and not self.path.endswith(".html")
+            and not self.path.endswith(".css")
+        ):
+            self.path = "/index.html"
+        return super().do_GET()
+
+
+if len(argv) < 3 or not argv[2].isnumeric():
+    print("WRONG USAGE!")
+    print("server.py ip port")
+else:
+    server = HTTPServer((argv[1], int(argv[2])), RequestHandler)
+    print(f"Server started at {argv[1]}:{argv[2]}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
