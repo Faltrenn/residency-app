b344368708b4774799bf3cfbaab6d4ba9a7bbfcd
manel <manelzaum@icloud.com>
2025-01-03 21:32:24 -0300

    rename serviceWork script.

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "residency-v3"; // Trocar sempre que tiver mudança em arquivos da aplicação
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./pages/login.html",
+  "./pages/home.html",
+  "./js/pages/login.js",
+  "./js/pages/home.js",
+  "./js/app.js",
+  "./images/icons/icon-192x192.png",
+  "./images/icons/icon-512x512.png",
+  "./images/icons/apple-touch-icon.png",
+  "./images/icons/favicon-96x96.png",
+  "./images/icons/favicon.ico",
+  "./images/icons/favicon.svg",
+];
