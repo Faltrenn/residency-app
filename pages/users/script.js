60b88334e7e0edd620597e71e03b9738c430a83c
manel <manelzaum@icloud.com>
2025-01-29 12:03:32 -0300

    add: remove user logic.

--- pages/users/script.js
+++ pages/users/script.js
@@ -1,21 +1,41 @@
-import { fetchUsers } from "../../src/js/utils.js";
+import { fetchAPI, fetchUsers } from "../../src/js/utils.js";
 
 /**
  * Executa sempre que a pagina users é carregada.
  *
  */
 export async function start() {
-  const ut = document.getElementById("users-table")
-  const data = await fetchUsers();
-  data.forEach(element => {
+  const ut = document.getElementById("users-table");
+  ut.getElementsByTagName("tbody")[0]?.remove();
+  const tbody = document.createElement("tbody");
+  ut.appendChild(tbody);
+
+  let data = await fetchUsers();
+  data.forEach((element) => {
     let tr = document.createElement("tr");
-    const keys = ["id", "name", "institution", "role", "pass"]
+    const keys = ["id", "name", "institution", "role", "pass"];
     for (const key of keys) {
       let td = document.createElement("td");
       td.textContent = element[key];
 
       tr.appendChild(td);
     }
-    ut.appendChild(tr);
+
+    let td = document.createElement("td");
+    let btn = document.createElement("button");
+    btn.onclick = () => {
+      deleteUser(element["id"]);
+    };
+    btn.textContent = "DEL";
+    td.appendChild(btn);
+    tr.classList.add("tdata");
+
+    tr.appendChild(td);
+    tbody.appendChild(tr);
   });
+}
+
+export async function deleteUser(id) {
+  await fetchAPI("/users", "DELETE", {}, { id: id });
+  await start();
 }
