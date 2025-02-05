3bd0b7fd139846fd84ef8f66148a66a37924e7e6
manel <manelzaum@icloud.com>
2025-02-05 09:53:07 -0300

    add: delete question.

--- pages/questions/script.js
+++ pages/questions/script.js
@@ -1,5 +1,5 @@
 import { navigate } from "../../src/js/router.js";
-import { fetchQuestions } from "../../src/js/utils.js";
+import { fetchAPI, fetchQuestions } from "../../src/js/utils.js";
 
 export async function questionsStart() {
   const ql = document.getElementById("questions-list");
@@ -8,12 +8,12 @@ export async function questionsStart() {
   questions.forEach((question) => {
     let q = document.createElement("h2");
 
-    //let btn = document.createElement("button");
-    //btn.onclick = () => {
-    //  deleteQuestion(element["id"]);
-    //};
-    //btn.textContent = "DEL";
-    //ql.appendChild(btn);
+    let btn = document.createElement("button");
+    btn.onclick = () => {
+      deleteQuestion(question["id"]);
+    };
+    btn.textContent = "DEL";
+    ql.appendChild(btn);
 
     let btn2 = document.createElement("button");
     btn2.onclick = () => {
@@ -34,4 +34,8 @@ export async function questionsStart() {
   });
 }
 
+export async function deleteQuestion(id) {
+  await fetchAPI("/questions", "DELETE", {}, { id: id });
+  await questionsStart();
+}
 
