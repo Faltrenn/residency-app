d6de309df95bde5cbd278a9d35a589352505cc87
manel <manelzaum@icloud.com>
2025-02-09 20:39:27 -0300

    fix: update role.

--- pages/update-role/update-role.js
+++ pages/update-role/update-role.js
@@ -1,3 +1,5 @@
+import { fetchAPI } from "../../src/js/utils.js";
+
 export async function startUpdateRole(role) {
   document
     .getElementById("add-role-form")
@@ -7,4 +9,22 @@ export async function startUpdateRole(role) {
 
   document.getElementById("last_title").value = role["title"];
   document.getElementById("title").value = role["title"];
+}
+
+export async function updateRole(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const last_title = document.getElementById("last_title").value;
+
+  try {
+    await fetchAPI(
+      "/roles",
+      "PUT",
+      {},
+      { title: title, last_title: last_title },
+    );
+  } catch (error) {
+    alert(`Não foi possível adicionar o cargo. Tente novamente.\n${error}`);
+  }
 }
